080c972a3394b229871cb955532f01c32bc830a6
AdityaVSM <adityavsm.is19@bmsce.ac.in>
2021-05-31 23:12:43 +0530

    added dummy api for json parsing

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +10,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async{
+    Uri url = Uri.parse("https://jsonplaceholder.typicode.com/todos/1");
+    Response response = await get(url);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
